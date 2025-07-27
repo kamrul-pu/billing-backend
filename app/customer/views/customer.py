b2d11049ce9d8865e21df01ec2110bb1b80ea15d
Kamrul Hasan <kamrul@Kamruls-MacBook-Air.local>
2025-07-27 23:58:06 +0600

    filter added in customer and payments

--- app/customer/views/customer.py
+++ app/customer/views/customer.py
@@ -19,9 +19,25 @@ from customer.serializers.payment import PaymentListSerializer
 
 
 class CustomerList(ListCreateAPIView):
-    queryset = Customer().get_all_actives()
     serializer_class = CustomerListSerializer
     permission_classes = [IsAdminUser | IsManager | IsStaff]
+
+    def get_queryset(self):
+        queryset = Customer().get_all_actives()
+        name: str = self.request.query_params.get("name", None)
+        user_id: int = self.request.query_params.get("user_id", None)
+        phone = self.request.query_params.get("phone", None)
+        package_id = self.request.query_params.get("package_id", None)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        if phone:
+            queryset = queryset.filter(phone=phone)
+        if package_id:
+            queryset = queryset.filter(package_id=package_id)
+
+        return queryset
 
 
 class CustomerDetail(RetrieveUpdateDestroyAPIView):
--- app/customer/views/payment.py
+++ app/customer/views/payment.py
@@ -16,9 +16,22 @@ from customer.serializers.payment import (
 
 
 class PaymentsList(ListCreateAPIView):
-    queryset = Payment().get_all_actives()
+    # queryset = Payment().get_all_actives()
     serializer_class = PaymentListSerializer
     permission_classes = [IsAdminUser | IsManager | IsStaff]
+
+    def get_queryset(self):
+        queryset = Payment().get_all_actives()
+        customer_name = self.request.query_params.get("customer_name", None)
+        customer_phone = self.request.query_params.get("customer_phone", None)
+        if customer_phone:
+            queryset = (
+                Payment().get_all_actives().filter(customer__phone=customer_phone)
+            )
+
+        if customer_name:
+            queryset = queryset.filter(customer__name__icontains=customer_name)
+        return queryset
 
 
 class PaymentDetail(RetrieveUpdateDestroyAPIView):
