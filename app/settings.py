e19c387d18dbc87e0acf4609b7ff1cefb79d6dd5
Kamrul Hasan <kamrul@evidentbd.com>
2025-08-09 17:15:38 +0600

    added trusted origins

--- app/settings.py
+++ app/settings.py
@@ -48,6 +48,10 @@ ENABLE_SILK = False
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "DJANGO_CSRF_TRUSTED_ORIGINS", "https://127.0.0.1"
+).split(",")
+
 
 # Application definition
 
