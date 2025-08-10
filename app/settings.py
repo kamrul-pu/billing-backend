deb373ec507b28092261a4b34d7deb46c744b392
Kamrul Hasan <kamrul@Kamruls-MacBook-Air.local>
2025-08-10 19:15:27 +0600

    made debug as Hardcoded False

--- app/settings.py
+++ app/settings.py
@@ -41,8 +41,9 @@ MEDIA_DIR = os.path.realpath(os.path.join(HOME_DIR, "media"))
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", False))
+# DEBUG = bool(os.environ.get("DEBUG", False))
 # print(f"DEBUG: {DEBUG}")
+DEBUG = False
 
 ENABLE_SILK = os.environ.get("ENABLE_SILK", False)
 
