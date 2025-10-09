878861e0e3fb6c3d35ad2e4f6886f37862783847
Kamrul Hasan <75220842+kamrulh4@users.noreply.github.com>
2025-10-09 11:57:48 +0600

    fix docker issue

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Applying migrations..."
 python manage.py migrate --noinput
-
-echo "Starting server on port 8001..."
-exec gunicorn --bind 0.0.0.0:8001 --workers 3 --threads 2 app.wsgi:application
+gunicorn --bind 0.0.0.0:8000 --workers 3 --threads 2 app.wsgi:application
