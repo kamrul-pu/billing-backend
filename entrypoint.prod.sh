f0f1b4a58347f01c0ea87ccbed9738af2dbd7c72
Kamrul Hasan <75220842+kamrulh4@users.noreply.github.com>
2025-10-14 13:21:57 +0600

    fix

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,10 +1,5 @@
-# Create fresh file
-cat > entrypoint.prod.sh << 'EOF'
 #!/usr/bin/env bash
 
+# python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 gunicorn --bind 0.0.0.0:8000 --workers 3 --threads 2 app.wsgi:application
-EOF
-
-# Make it executable
-chmod +x entrypoint.prod.sh
