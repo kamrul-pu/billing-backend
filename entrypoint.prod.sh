77a66809b35f1bc31238c85eb2a038197153d31b
Kamrul Hasan <75220842+kamrulh4@users.noreply.github.com>
2025-10-14 13:05:04 +0600

    update entrypoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,5 +1,23 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
+# gunicorn --bind 0.0.0.0:8000 --workers 3 --threads 2 app.wsgi:application
+
+#!/bin/bash
+
+# Ensure staticfiles dir is writable
+echo "Fixing permissions for /app/staticfiles..."
+chown -R appuser:appuser /app/staticfiles
+
+# Run migrations
+echo "Running migrations..."
 python manage.py migrate --noinput
-gunicorn --bind 0.0.0.0:8000 --workers 3 --threads 2 app.wsgi:application
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+gunicorn app.wsgi:application --bind 0.0.0.0:8000 --workers 3 --threads 3
