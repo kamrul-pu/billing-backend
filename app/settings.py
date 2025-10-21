2e24367316575fe0df6c0826a63b11dd11c37239
Kamrul Hasan <kamrul@evidentbd.com>
2025-10-21 19:26:47 +0600

    beta release with nginx

--- app/settings.py
+++ app/settings.py
@@ -148,7 +148,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 # DATABASE_URL = os.environ.get(
 #     "DATABASE_URL", "postgres://dev_user:changeme@db:5432/dev_db"
 # )
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
 
 DATABASES = {
     "default": dj_database_url.config(
